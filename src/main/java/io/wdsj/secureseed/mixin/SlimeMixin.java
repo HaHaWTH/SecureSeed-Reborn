db761c39fa3f7875a41f0c2e293d86843cf1c3b7
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2024-12-02 21:10:57 +0800

    SlimeMixin

--- src/main/java/io/wdsj/secureseed/mixin/SlimeMixin.java
+++ src/main/java/io/wdsj/secureseed/mixin/SlimeMixin.java
@@ -9,16 +9,15 @@ import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.LevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Slime.class)
 public abstract class SlimeMixin implements IChunkAccessSlimeChunk {
-    @Inject(
+    @ModifyVariable(
             method = "checkSlimeSpawnRules",
-            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/levelgen/WorldgenRandom;seedSlimeChunk(IIJJ)Lnet/minecraft/util/RandomSource;"),
-            cancellable = true)
-    private static void check(EntityType<Slime> entityType, LevelAccessor levelAccessor, EntitySpawnReason entitySpawnReason, BlockPos blockPos, RandomSource randomSource, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(((IChunkAccessSlimeChunk)(Object)levelAccessor.getChunk(blockPos.getX() >> 4, blockPos.getZ() >> 4)).secureSeed$isSlimeChunk());
+            at = @At(value = "STORE", target = "Lnet/minecraft/world/level/levelgen/WorldgenRandom;seedSlimeChunk(IIJJ)Lnet/minecraft/util/RandomSource;")
+    )
+    private static boolean replace(boolean value, EntityType<Slime> entityType, LevelAccessor levelAccessor, EntitySpawnReason entitySpawnReason, BlockPos blockPos, RandomSource randomSource) {
+        return ((IChunkAccessSlimeChunk)(Object)levelAccessor.getChunk(blockPos.getX() >> 4, blockPos.getZ() >> 4)).secureSeed$isSlimeChunk();
     }
 }
